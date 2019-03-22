ccdc5d970b9e502b20fb60bd7fe6b79095a67b23
ybcc <271380178@qq.com>
2019-03-22 15:56:41 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+dataset_name = 'shtechA'
+
+output_dir = './saved_models/'
+
+train_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/train'
+train_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/train_den'
+
+val_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val'
+val_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val_den'
---
+++ utils/data_generater.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 _*-
+"""
+@author: steven.yi
+@date: 2019/3/22
+@file: data_generater.py
+@description: 数据生成器
+"""
