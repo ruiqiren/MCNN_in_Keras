de4fd8cfb1c8fb64c913e0fccedb18fa8c888452
ybcc <271380178@qq.com>
2019-03-25 17:40:41 +0800

    add data generater

--- utils/data_generater.py
+++ utils/data_generater.py
@@ -3,5 +3,60 @@
 @author: steven.yi
 @date: 2019/3/22
 @file: data_generater.py
-@description: 数据生成器
+@description: 生成训练数据
 """
+import numpy as np
+import cv2
+import os
+import random
+import pandas as pd
+
+
+class DataLoader(object):
+    def __init__(self, data_path, gt_path, shuffle=False, gt_downsample=False, pre_load=False):
+        self.data_path = data_path
+        self.gt_path = gt_path
+        self.shuffle = shuffle
+        self.gt_downsample = gt_downsample
+        self.pre_load = pre_load
+        self.data_files = [filename for filename in os.listdir(data_path)]
+        self.num_samples = len(self.data_files)
+        self.blob_list = []
+
+        for fname in self.data_files:
+            img = cv2.imread(os.path.join(self.data_path, fname), 0)
+            img = img.astype(np.float32, copy=False)
+            ht = img.shape[0]
+            wd = img.shape[1]
+            ht_1 = int((ht / 4) * 4)
+            wd_1 = int((wd / 4) * 4)
+            img = cv2.resize(img, (wd_1, ht_1))
+            img = img.reshape((img.shape[0], img.shape[1], 1))
+            den = pd.read_csv(os.path.join(self.gt_path, os.path.splitext(fname)[0] + '.csv'),
+                              header=None).as_matrix()
+            den = den.astype(np.float32, copy=False)
+            if self.gt_downsample:
+                wd_1 = int(wd_1 / 4)
+                ht_1 = int(ht_1 / 4)
+                den = cv2.resize(den, (wd_1, ht_1))
+                den = den * ((wd * ht) / (wd_1 * ht_1))
+            else:
+                den = cv2.resize(den, (wd_1, ht_1))
+                den = den * ((wd * ht) / (wd_1 * ht_1))
+            den = den.reshape((den.shape[0], den.shape[1], 1))
+            blob = dict()
+            blob['data'] = img
+            blob['gt'] = den
+            self.blob_list.append(blob)
+
+        if self.shuffle:
+            np.random.shuffle(self.blob_list)
+
+    def flow(self, batch_size=32):
+        loop_count = self.num_samples // batch_size
+        while True:
+            i = np.random.randint(0, loop_count)
+            blobs = self.blob_list[i*batch_size: (i+1)*batch_size]
+            X_batch = np.array([blob['data'] for blob in blobs])
+            Y_batch = np.array([blob['gt'] for blob in blobs])
+            yield X_batch, Y_batch
