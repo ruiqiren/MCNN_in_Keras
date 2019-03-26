ab17c3bb925e88af12e002d872412a2d72ea7893
ybcc <271380178@qq.com>
2019-03-26 16:35:50 +0800

    add some global configs

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 dataset_name = 'shtechA'
 
-output_dir = './saved_models/'
+output_dir = './trained_models/'
 
-train_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/train'
-train_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/train_den'
+train_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
+train_gt_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
+val_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
+val_gt_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
 
-val_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val'
-val_gt_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/val_den'
+Epochs = 20
+Train_Batch_Size = 32
+Val_Batch_Size = 1
