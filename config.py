64af10791fad43ea57b9062df8c7fe7ae447b5ec
ybcc <271380178@qq.com>
2019-03-27 16:06:17 +0800

    some fix

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
-dataset_name = 'shtechA'
+MODEL_DIR = './trained_models/'
 
-output_dir = './trained_models/'
+TRAIN_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
 
-train_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
-train_gt_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
-val_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
-val_gt_path = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
+TRAIN_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
 
-Epochs = 20
-Train_Batch_Size = 32
-Val_Batch_Size = 1
+VAL_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
+
+VAL_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
+
+TEST_PATH = './data/original/shanghaitech/part_{}_final/test_data/images/'
+
+TEST_GT_PATH = './data/original/shanghaitech/part_{}_final/test_data/ground_truth_csv/'
+
+EPOCHS = 20
+
+TRAIN_BATCH_SIZE = 32
+
+VAL_BATCH_SIZE = 1
