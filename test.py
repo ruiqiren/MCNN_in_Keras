73cddc0f3de21c6afc11b1f1bebdcae6cd6b18cc
ybcc <271380178@qq.com>
2019-03-28 21:13:38 +0800

    rebuild some code

--- test.py
+++ test.py
@@ -9,65 +9,74 @@ from keras.models import load_model
 from utils.data_loader import DataLoader
 import numpy as np
 import config as cfg
-import sys
+import argparse
 import os
 import cv2
 
 
 def save_density_map(save_dir, density_map, fname='results.png'):
+    """
+    保存预测密度图
+    :param save_dir: 保存目录
+    :param density_map: 密度图矩阵
+    :param fname: 保存文件名
+    :return:
+    """
     density_map = 255*density_map/np.max(density_map)
     density_map = density_map[0][0]
     cv2.imwrite(os.path.join(save_dir, fname), density_map)
 
 
-if len(sys.argv) == 2:
-    dataset = sys.argv[1]
-else:
-    print('usage: python test.py A(or B)')
-    exit()
-print('Testing Part_{} ...'.format(dataset))
+def main(args):
+    dataset = args.dataset  # 'A' or 'B'
+    if dataset == 'A':
+        model_path = './trained_models/mcnn_A_train.hdf5'
+    else:
+        model_path = './trained_models/mcnn_B_train.hdf5'
 
-test_path = cfg.TEST_PATH.format(dataset)
-test_gt_path = cfg.TEST_GT_PATH.format(dataset)
+    output_dir = './output_{}/'.format(dataset)
+    density_maps_dir = os.path.join(output_dir, 'density_maps')  # directory to save predicted density map
+    results_txt = os.path.join(output_dir, 'results.txt')  # file to save predicted results
+    for _dir in [output_dir, density_maps_dir]:
+        if not os.path.exists(_dir):
+            os.mkdir(_dir)
 
-if dataset == 'A':
-    model_path = './trained_models/mcnn_A_train.hdf5'
-else:
-    model_path = './trained_models/mcnn_B_train.hdf5'
+    test_path = cfg.TEST_PATH.format(dataset)
+    test_gt_path = cfg.TEST_GT_PATH.format(dataset)
+    # load test set
+    data_loader = DataLoader(test_path, test_gt_path, shuffle=False, gt_downsample=True)
+    # load model
+    model = load_model(model_path)
 
-output_dir = './output_{}/'.format(dataset)
-density_maps_dir = os.path.join(output_dir, 'density_maps')
-results_txt = os.path.join(output_dir, 'results.txt')
+    # test
+    print('Testing Part_{} ...'.format(dataset))
+    mae = 0.0
+    mse = 0.0
+    for blob in data_loader:
+        img = blob['data']
+        gt = blob['gt']
+        pred = model.predict(np.expand_dims(img, axis=0))
+        gt_count = np.sum(gt)
+        pred_count = np.sum(pred)
+        mae += abs(gt_count - pred_count)
+        mse += ((gt_count - pred_count) * (gt_count - pred_count))
+        # # save density map
+        # save_density_map(density_maps_dir, pred, blob['fname'].split('.')[0] + '.png')
 
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-if not os.path.exists(density_maps_dir):
-    os.mkdir(density_maps_dir)
+        # save results
+        with open(results_txt, 'a') as f:
+            line = '<{}> {:.2f} -- {:.2f}\n'.format(blob['fname'].split('.')[0], gt_count, pred_count)
+            f.write(line)
 
-model = load_model(model_path)
-data_loader = DataLoader(test_path, test_gt_path, shuffle=False, gt_downsample=True)
-
-mae = 0.0
-mse = 0.0
-for blob in data_loader:
-    img = blob['data']
-    gt = blob['gt']
-    pred = model.predict(np.expand_dims(img, axis=0))
-    gt_count = np.sum(gt)
-    pred_count = np.sum(pred)
-    mae += abs(gt_count - pred_count)
-    mse += ((gt_count - pred_count) * (gt_count - pred_count))
-    # # save density map
-    # save_density_map(density_maps_dir, pred, blob['fname'].split('.')[0] + '.png')
-
-    # save results
+    mae = mae / data_loader.num_samples
+    mse = np.sqrt(mse / data_loader.num_samples)
+    print('MAE: %0.2f, MSE: %0.2f' % (mae, mse))
     with open(results_txt, 'a') as f:
-        line = '<{}> {:.2f} -- {:.2f}\n'.format(blob['fname'].split('.')[0], gt_count, pred_count)
-        f.write(line)
+        f.write('MAE: %0.2f, MSE: %0.2f' % (mae, mse))
 
-mae = mae/data_loader.num_samples
-mse = np.sqrt(mse/data_loader.num_samples)
-print('\nMAE: %0.2f, MSE: %0.2f' % (mae, mse))
 
-with open(results_txt, 'a') as f:
-    f.write('MAE: %0.2f, MSE: %0.2f' % (mae, mse))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="the dataset you want to predict", choices=['A', 'B'])
+    args = parser.parse_args()
+    main(args)
--- train.py
+++ train.py
@@ -5,60 +5,63 @@
 @file: train.py
 @description: 训练
 """
-from keras.optimizers import Adam, SGD
+from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from model import MCNN
 from utils.data_loader import DataLoader
 import config as cfg
-import sys
 import os
 from metrics import mae, mse
+import argparse
 
 
-if len(sys.argv) == 2:
-    dataset = sys.argv[1]
-else:
-    print('usage: python3 train.py A(or B)')
-    exit()
+def main(args):
+    dataset = args.dataset  # 'A' or 'B'
 
-train_path = cfg.TRAIN_PATH.format(dataset)
-train_gt_path = cfg.TRAIN_GT_PATH.format(dataset)
-val_path = cfg.VAL_PATH.format(dataset)
-val_gt_path = cfg.VAL_GT_PATH.format(dataset)
+    train_path = cfg.TRAIN_PATH.format(dataset)
+    train_gt_path = cfg.TRAIN_GT_PATH.format(dataset)
+    val_path = cfg.VAL_PATH.format(dataset)
+    val_gt_path = cfg.VAL_GT_PATH.format(dataset)
+    # 生成训练数据和验证数据
+    train_data_gen = DataLoader(train_path, train_gt_path, shuffle=True, gt_downsample=True)
+    val_data_gen = DataLoader(val_path, val_gt_path, shuffle=False, gt_downsample=True)
+    x_train, y_train = train_data_gen.get_all()
+    x_val, y_val = val_data_gen.get_all()
 
-train_data_gen = DataLoader(train_path, train_gt_path, shuffle=True, gt_downsample=True)
-val_data_gen = DataLoader(val_path, val_gt_path, shuffle=False, gt_downsample=True)
+    # 定义模型
+    input_shape = (None, None, 1)
+    model = MCNN(input_shape)
+    adam = Adam(lr=1e-4)
+    model.compile(loss='mse', optimizer=adam, metrics=[mae, mse])
 
-input_shape = (None, None, 1)
-model = MCNN(input_shape)
-# adam = Adam(lr=1e-4)
-sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9)
-model.compile(loss='mse', optimizer=sgd, metrics=[mae, mse])
+    # 定义callback
+    checkpointer_best_train = ModelCheckpoint(
+        filepath=os.path.join(cfg.MODEL_DIR, 'mcnn_'+dataset+'_train.hdf5'),
+        monitor='loss', verbose=1, save_best_only=True, mode='min'
+    )
+    lr_reducer = ReduceLROnPlateau(monitor='loss', factor=0.1,
+                                   cooldown=0, patience=10, min_lr=0)
+    callback_list = [checkpointer_best_train, lr_reducer]
 
-checkpointer_best_train = ModelCheckpoint(
-    filepath=os.path.join(cfg.MODEL_DIR, 'mcnn_'+dataset+'_train.hdf5'),
-    monitor='loss', verbose=1, save_best_only=True, mode='min'
-)
-lr_reducer = ReduceLROnPlateau(monitor='loss', factor=0.1,
-                               cooldown=0, patience=10, min_lr=0)
+    # 训练
+    print('Training Part_{} ...'.format(dataset))
+    model.fit(
+        x=x_train, y=y_train, batch_size=1, epochs=cfg.EPOCHS,
+        validation_data=(x_val, y_val),
+        callbacks=callback_list
+    )
 
-callback_list = [checkpointer_best_train, lr_reducer]
+    # model.fit_generator(train_data_gen.flow(cfg.TRAIN_BATCH_SIZE),
+    #                     steps_per_epoch=train_data_gen.num_samples // cfg.TRAIN_BATCH_SIZE,
+    #                     validation_data=val_data_gen.flow(cfg.VAL_BATCH_SIZE),
+    #                     validation_steps=val_data_gen.num_samples // cfg.VAL_BATCH_SIZE,
+    #                     epochs=cfg.EPOCHS,
+    #                     callbacks=callback_list,
+    #                     verbose=1)
 
-print('Training Part_{} ...'.format(dataset))
-model.fit_generator(train_data_gen.flow(cfg.TRAIN_BATCH_SIZE),
-                    steps_per_epoch=train_data_gen.num_samples // cfg.TRAIN_BATCH_SIZE,
-                    validation_data=val_data_gen.flow(cfg.VAL_BATCH_SIZE),
-                    validation_steps=val_data_gen.num_samples // cfg.VAL_BATCH_SIZE,
-                    epochs=cfg.EPOCHS,
-                    callbacks=callback_list,
-                    verbose=1)
 
-# x_train, y_train = train_data_gen.get_all()
-# x_val, y_val = val_data_gen.get_all()
-#
-# print('Training Part_{} ...'.format(dataset))
-# history = model.fit(
-#     x=x_train, y=y_train, batch_size=1, epochs=cfg.EPOCHS,
-#     validation_data=(x_val, y_val),
-#     callbacks=callback_list
-# )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="the dataset you want to train", choices=['A', 'B'])
+    args = parser.parse_args()
+    main(args)
