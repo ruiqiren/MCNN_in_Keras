ac065231d8c92f528d6dcfcebb8432b0627b87e9
ybcc <271380178@qq.com>
2019-03-28 21:13:59 +0800

    add comments

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 # This file is used to save the global configurations
 
+# 模型存放目录
 MODEL_DIR = './trained_models/'
 
-TRAIN_PATH = '/opt/dataset/crowd_counting/data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
+# 训练集目录
+TRAIN_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
+# 训练集Ground-Truth目录
+TRAIN_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
 
-TRAIN_GT_PATH = '/opt/dataset/crowd_counting/data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
+# 验证集目录
+VAL_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
+# 验证集Ground_Truth目录
+VAL_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
 
-VAL_PATH = '/opt/dataset/crowd_counting/data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
-
-VAL_GT_PATH = '/opt/dataset/crowd_counting/data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
-
-TEST_PATH = '/opt/dataset/crowd_counting/data/original/shanghaitech/part_{}_final/test_data/images/'
-
-TEST_GT_PATH = '/opt/dataset/crowd_counting/data/original/shanghaitech/part_{}_final/test_data/ground_truth_csv/'
+# 测试集目录
+TEST_PATH = './data/original/shanghaitech/part_{}_final/test_data/images/'
+# 测试集Ground_Truth目录
+TEST_GT_PATH = './data/original/shanghaitech/part_{}_final/test_data/ground_truth_csv/'
 
 EPOCHS = 200
-
 TRAIN_BATCH_SIZE = 32
-
 VAL_BATCH_SIZE = 1
