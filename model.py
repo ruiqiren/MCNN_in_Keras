6036f0174d333fc0761d9a23ca87ff246d22bc0d
ybcc <271380178@qq.com>
2019-03-28 21:19:24 +0800

    remove BatchNormlization layer

--- model.py
+++ model.py
@@ -6,56 +6,38 @@
 @description: MCNN 模型定义
 """
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Input, Concatenate, Activation, BatchNormalization
-
-
-def BN_and_Relu(input_tensor):
-    output_tensor = BatchNormalization()(input_tensor)
-    output_tensor = Activation('relu')(output_tensor)
-    return output_tensor
+from keras.layers import Conv2D, MaxPooling2D, Input, Concatenate
 
 
 def MCNN(input_shape=None):
     inputs = Input(shape=input_shape)
 
     # column 1
-    column_1 = Conv2D(16, (9, 9), padding='same')(inputs)
-    column_1 = BN_and_Relu(column_1)
+    column_1 = Conv2D(16, (9, 9), padding='same', activation='relu')(inputs)
     column_1 = MaxPooling2D(2)(column_1)
     column_1 = (column_1)
-    column_1 = Conv2D(32, (7, 7), padding='same')(column_1)
-    column_1 = BN_and_Relu(column_1)
+    column_1 = Conv2D(32, (7, 7), padding='same', activation='relu')(column_1)
     column_1 = MaxPooling2D(2)(column_1)
-    column_1 = Conv2D(16, (7, 7), padding='same')(column_1)
-    column_1 = BN_and_Relu(column_1)
-    column_1 = Conv2D(8, (7, 7), padding='same')(column_1)
-    column_1 = BN_and_Relu(column_1)
+    column_1 = Conv2D(16, (7, 7), padding='same', activation='relu')(column_1)
+    column_1 = Conv2D(8, (7, 7), padding='same', activation='relu')(column_1)
 
     # column 2
-    column_2 = Conv2D(20, (7, 7), padding='same')(inputs)
-    column_2 = BN_and_Relu(column_2)
+    column_2 = Conv2D(20, (7, 7), padding='same', activation='relu')(inputs)
     column_2 = MaxPooling2D(2)(column_2)
     column_2 = (column_2)
-    column_2 = Conv2D(40, (5, 5), padding='same')(column_2)
-    column_2 = BN_and_Relu(column_2)
+    column_2 = Conv2D(40, (5, 5), padding='same', activation='relu')(column_2)
     column_2 = MaxPooling2D(2)(column_2)
-    column_2 = Conv2D(20, (5, 5), padding='same')(column_2)
-    column_2 = BN_and_Relu(column_2)
-    column_2 = Conv2D(10, (5, 5), padding='same')(column_2)
-    column_2 = BN_and_Relu(column_2)
+    column_2 = Conv2D(20, (5, 5), padding='same', activation='relu')(column_2)
+    column_2 = Conv2D(10, (5, 5), padding='same', activation='relu')(column_2)
 
     # column 3
-    column_3 = Conv2D(24, (5, 5), padding='same')(inputs)
-    column_3 = BN_and_Relu(column_3)
+    column_3 = Conv2D(24, (5, 5), padding='same', activation='relu')(inputs)
     column_3 = MaxPooling2D(2)(column_3)
     column_3 = (column_3)
-    column_3 = Conv2D(48, (3, 3), padding='same')(column_3)
-    column_3 = BN_and_Relu(column_3)
+    column_3 = Conv2D(48, (3, 3), padding='same', activation='relu')(column_3)
     column_3 = MaxPooling2D(2)(column_3)
-    column_3 = Conv2D(24, (3, 3), padding='same')(column_3)
-    column_3 = BN_and_Relu(column_3)
-    column_3 = Conv2D(12, (3, 3), padding='same')(column_3)
-    column_3 = BN_and_Relu(column_3)
+    column_3 = Conv2D(24, (3, 3), padding='same', activation='relu')(column_3)
+    column_3 = Conv2D(12, (3, 3), padding='same', activation='relu')(column_3)
 
     # merge feature map of 3 columns in last dimension
     merges = Concatenate(axis=-1)([column_1, column_2, column_3])
